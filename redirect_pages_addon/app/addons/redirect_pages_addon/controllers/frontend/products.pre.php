db8ddbbf87f56d04d6ec84e62188922c4f54584d
thanakit12.40 <thanakit.40@gmail.com>
2019-07-08 19:31:45 +0700

    edit redirect_pages_addon in review_1

--- redirect_pages_addon/app/addons/redirect_pages_addon/controllers/frontend/products.pre.php
+++ redirect_pages_addon/app/addons/redirect_pages_addon/controllers/frontend/products.pre.php
@@ -9,13 +9,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if($mode == "view") {
 
     // Variable name should be relevant and consistent. For example: $pro_id should be $product_id
-    $pro_id = $_REQUEST["product_id"];
-    $is_exists = fn_product_exists($pro_id);
+    $product_id = $_REQUEST["product_id"];
+    $is_exists = fn_product_exists($product_id);
     if($is_exists)
     {
-        $is_disable = fn_redirect_pages_addon_is_disable($pro_id);
+        $is_disable = fn_redirect_pages_addon_is_disable($product_id);
         if($is_disable) {
-            $main_category_id = fn_redirect_pages_addon_get_main_category_id($pro_id);
+            $main_category_id = fn_redirect_pages_addon_get_main_category_id($product_id);
             fn_redirect("categories.view?category_id=".$main_category_id);
         }
     }
--- redirect_pages_addon/app/addons/redirect_pages_addon/func.php
+++ redirect_pages_addon/app/addons/redirect_pages_addon/func.php
@@ -8,21 +8,12 @@ if ( !defined('AREA') ) { die('Access denied'); }
 //    - Use appropriate function for expecpted result. For example: result return one field, one row or multiple rows
 function fn_redirect_pages_addon_is_disable($product_id)
 {
-    $query = db_get_array("select status from ?:products where product_id = '$product_id'");
-    if($query[0]["status"] == "D") {
-        return true;
-    }
-    else
-        return false;
+    $query = db_get_row("select status from ?:products where product_id = ?i",$product_id);
+    return ($query["status"] == "D") ? true : false;
 }
 function fn_redirect_pages_addon_get_main_category_id($product_id)
 {
-    $query = db_get_array("SELECT link_type,category_id FROM  ?:products_categories WHERE ?:products_categories.product_id = $product_id");
-
-    // Why should we have 'for loop' for result that guarantee one row?
-    for($i = 0 ; $i < count($query) ; $i++)
-    {
-        if($query[$i]["link_type"] == 'M')
-            return $query[$i]["category_id"];
-    }
+    $query = db_get_row("SELECT link_type,category_id FROM  ?:products_categories WHERE ?:products_categories.product_id = ?i 
+                            and link_type = 'M'",$product_id);
+    return $query["category_id"];
 }
