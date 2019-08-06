1e438cf5158fa63889c3f2dc20365b977169bcb3
thanakit12.40 <thanakit.40@gmail.com>
2019-08-06 17:50:50 +0700

    order_export_v.1.1

--- order_export/app/addons/order_export/func.php
+++ order_export/app/addons/order_export/func.php
@@ -1,6 +1,7 @@
 <?php
-if ( !defined('AREA') ) { die('Access denied'); }
-
+if (!defined('AREA')) {
+    die('Access denied');
+}
 
 function fn_order_export_get_product_name($item_id)
 {
@@ -9,7 +10,78 @@ function fn_order_export_get_product_name($item_id)
                         ?:product_descriptions
                         ON
                         ?:order_details.product_id = ?:product_descriptions.product_id
-                        WHERE ?:order_details.item_id in (?n)
-                        and ?:product_descriptions.lang_code = 'th'",$item_id);
-   return $query['product'];
+                        WHERE ?:order_details.item_id = ?i
+                        and ?:product_descriptions.lang_code = 'th'", $item_id);
+    return $query['product'];
+}
+
+function fn_order_export_timestamp_to_date($timestamp)
+{
+    return !empty($timestamp) ? date('d/m/Y H:i:s', intval($timestamp)) : '';
+}
+
+function fn_order_export_get_status_desc($order_id)
+{
+
+    $query = db_get_row("select description from ?:orders INNER JOIN
+             ?:statuses
+              ON
+             ?:statuses.status = ?:orders.status
+              INNER JOIN
+              ?:status_descriptions
+              ON
+              ?:status_descriptions.status_id = ?:statuses.status_id
+              WHERE
+              order_id = ?i
+              and ?:status_descriptions.lang_code = 'th'
+              and type = 'O'", $order_id);
+
+    return $query['description'];
+}
+
+function fn_order_export_order_history($order_id)
+{
+    $params = array(
+        "order_id" => $order_id,
+        "page" => "1"
+    );
+    $result = [];
+    $time = '';
+    $result = fn_get_order_history($params, 0);
+    $j = 0;
+    for ($i = 0; $i < count($result); $i++) {
+        $response = $result[$i][$j]["content"]["status_to"];
+        if ($response == 'C') {
+            $time = $result[$i][$j]["timestamp"];
+        }
+        $j++;
+    }
+
+    $finish_date = fn_order_export_timestamp_to_date($time);
+
+    return $finish_date;
+}
+
+function fn_generateCsv($data, $delimiter = ',', $enclosure = '"')
+{
+    $contents = '';
+    $handle = fopen('php://temp', 'r+');
+    foreach ($data as $line) {
+        fputcsv($handle, $line, $delimiter, $enclosure);
+    }
+    rewind($handle);
+    while (!feof($handle)) {
+        $contents .= fread($handle, 8192);
+    }
+    fclose($handle);
+    return $contents;
+}
+
+function fn_order_export_shipment_method($order_id)
+{
+    $query = db_get_row("SELECT shipping FROM ?:orders inner JOIN ?:shipping_descriptions ON ?:orders.shipping_ids = ?:shipping_descriptions.shipping_id 
+              WHERE ?:orders.order_id = ?i 
+              and ?:shipping_descriptions.lang_code = 'th'", $order_id);
+
+    return $query['shipping'];
 }
--- order_export/app/addons/order_export/schemas/exim/order_items.post.php
+++ order_export/app/addons/order_export/schemas/exim/order_items.post.php
@@ -4,11 +4,51 @@ use Tygh\Registry;
 
 include_once(Registry::get('config.dir.addons') . 'order_export/func.php');
 
-    $schema['export_fields']['Product Name'] = array(
-        'process_get' => array('fn_order_export_get_product_name','#key'),
-        'linked' => false,
-        'export_only' => true,
-    );
+$schema['export_fields']['Product Name'] = array(
+    'process_get' => array('fn_order_export_get_product_name', '#key'),
+    'linked' => false,
+    'export_only' => true,
+);
+
+$schema['export_fields']['First name'] = array(
+    'db_field' => 'firstname',
+    'table' => 'orders',
+);
+
+$schema['export_fields']['Last name'] = array(
+    'db_field' => 'lastname',
+    'table' => 'orders',
+);
+
+$schema['export_fields']['Date'] = array(
+    'db_field' => 'timestamp',
+    'table' => 'orders',
+    'process_get' => array('fn_order_export_timestamp_to_date', '#this'),
+//        'convert_put' => array('fn_date_to_timestamp', '#this'),
+);
+
+$schema['export_fields']['Status'] = array(
+    'db_field' => 'order_id',
+    'table' => 'orders',
+    'process_get' => array('fn_order_export_get_status_desc', '#this'),
+);
+
+$schema['export_fields']['Finish Date'] = array(
+    'db_field' => 'order_id',
+    'table' => 'orders',
+    'process_get' => array('fn_order_export_order_history', '#this'),
+);
+
+$schema['export_fields']['Shipment'] = array(
+    'db_field' => 'order_id',
+    'table' => 'orders',
+    'process_get' => array('fn_order_export_shipment_method', '#this'),
+);
+
+$schema['export_fields']['User ID'] = array(
+    'db_field' => 'user_id',
+    'table' => 'orders'
+);
 
 return $schema;
 
