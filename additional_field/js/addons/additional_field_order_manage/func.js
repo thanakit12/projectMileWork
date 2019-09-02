3807479a9296b824e3fc49266ef599aa30828dc6
thanakit12.40 <thanakit.40@gmail.com>
2019-09-02 18:20:02 +0700

    change in file js

--- additional_field/js/addons/additional_field_order_manage/func.js
+++ additional_field/js/addons/additional_field_order_manage/func.js
@@ -1,7 +1,6 @@
 
 // (function(_, $) {
     $(document).ready(function () {
-        console.log("DDDDDDDDDDDDDDD")
         $('.ty-phone').keypress(function (e) {
             if (e.which !== 8 && e.which !== 0 && (e.which < 48 || e.which > 57)) {
                 return false;
