10cb7a25cd7bae0ffbcd94413cb4f9d3b07b4011
thanakit12.40 <thanakit.40@gmail.com>
2019-11-04 19:29:08 +0700

    export_abandoned_carts edit

---
+++ export_abandoned_carts/app/addons/export_abandoned_carts/controllers/backend/export_abandoned_carts.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('BOOTSTRAP')) {
+    die('Access denied');
+}
+
+if ($mode == "export_range") {
+    if (!empty($_REQUEST['user_ids'])) {
+        if (empty(Tygh::$app['session']['export_ranges'])) {
+            Tygh::$app['session']['export_ranges'] = array();
+        }
+        if (empty(Tygh::$app['session']['export_ranges']['abandoned_carts'])) {
+            Tygh::$app['session']['export_ranges']['abandoned_carts'] = array('pattern_id' => 'abandoned_carts');
+        }
+        Tygh::$app['session']['export_ranges']['abandoned_carts']['data'] = array('user_id' => $_REQUEST['user_ids']);
+        unset($_REQUEST['redirect_url']);
+
+        return array(CONTROLLER_STATUS_REDIRECT, 'exim.export?section='abandoned_carts&pattern_id=' . Tygh::$app['session']['export_ranges']['abandoned_carts']['pattern_id']);
+    }
+}
