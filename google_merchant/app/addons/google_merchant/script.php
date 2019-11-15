c35bb49048bef5ba2520da1eab6be84d5340d21c
thanakit12.40 <thanakit.40@gmail.com>
2019-11-15 13:13:54 +0700

    edit script in google_merchant_v.1.1.7

--- google_merchant/app/addons/google_merchant/script.php
+++ google_merchant/app/addons/google_merchant/script.php
@@ -6,12 +6,14 @@ define('ACCOUNT_TYPE', 'admin');
 
 require_once('../../../init.php');
 
-$date = new DateTime();
 date_default_timezone_set("Asia/Bangkok");
-$dt_begin = $date->format('Y-m-d 00:00:00');
-$dt_end   = $date->format('Y-m-d 23:59:59');
+$date = new DateTime();
 
-fn_google_merchant_cron_job($dt_begin,$dt_end);
+//SET job at 24:00:00 of next day
+$date_begin = date("Y-m-d 00:00:00", strtotime("yesterday"));
+$date_end   = date("Y-m-d 23:59:59", strtotime("yesterday"));
+
+fn_google_merchant_cron_job($date_begin,$date_end);
 
 
 
